b2b7a715362945a2efc37fc3e0a73d682c2913b7
Danil <danilllah13579@gmail.com>
2025-10-16 22:13:11 +1000

    add @ResponseBody

--- src/main/java/com/app/cloudStorage/controller/auth/SignInController.java
+++ src/main/java/com/app/cloudStorage/controller/auth/SignInController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,8 +17,9 @@ public class SignInController {
     private final SignInService signInService;
 
     @GetMapping("/page")
+    @ResponseBody
     public String signInPage() {
-        return "welcome to the signIn page";
+        return "signIn";
     }
 
     @PostMapping("/auth")
